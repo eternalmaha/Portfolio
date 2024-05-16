df46b03d869a24686b4c26257f1523f516b20110
Maha <maha.ahmed4peace@gmail.com>
2024-05-16 16:39:46 -0400

    hero section boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import styles from './App.module.css'; 
 import { Navbar } from './components/Navbar/Navbar';
+import { Hero } from './components/Hero/Hero';
 
 function App() {
   return (
     <>
       <nav className={styles.App}>
         <Navbar /> 
+        <Hero /> 
       </nav>
     </>
   )
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import { getImageUrl } from '../../utils';
+import styles from "./Hero.module.css"
+
+export const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}> Hi, Maha here. </h1>
+                <p className={styles.description}> Full Stuck MERN Developer with 3 + experience. Reach out if you'd like to learn more. </p>
+                <a className = {styles.contactBtn} href = 'malito:myemail@gmail.com'> Contact Me </a>
+            </div>
+            <img className = {styles.Img} src = {getImageUrl("hero/heroImage.png")} alt = "Hero image of me"/> 
+            <div className={styles.topBlur}></div>
+            <div className={styles.bottomBlur}></div> 
+        </section>
+    )
+}
