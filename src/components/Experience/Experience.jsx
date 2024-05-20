a8b96694a774f40e5fd9af9d3fda274662f0c9cc
Maha <maha.ahmed4peace@gmail.com>
2024-05-20 13:43:37 -0400

    adding classes to experience section for styling

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,15 +1,16 @@
 import React from 'react'; 
-import skills from '../../data/skills.json'
+import skills from '../../data/skills.json';
 import { getImageUrl } from '../../utils';
-import history from "../../data/history.json"
+import history from "../../data/history.json";
+import styles from "./Experience.module.css"
 
 export const Experience = () => {
     return (
         <>
-            <section id='experience'>
-                <h2> Experience </h2>
-                <div>
-                    <div> {
+            <section className={styles.container} id='experience'>
+                <h2 className={styles.title}> Experience </h2>
+                <div className={styles.content}>
+                    <div className={styles.skills}> {
                         skills.map((skill,id) => {
                         
                             return (
@@ -26,12 +27,12 @@ export const Experience = () => {
                         }
                         
                     </div>
-                    <ul>
+                    <ul className={styles.history}>
                         {
                           history.map((historyItem,id) => {
-                            return <li key={id}>
+                            return <li className={styles.historyItem} key={id}>
                                 <img src= {getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`}/>
-                                <div>
+                                <div className={styles.historyItemDetails}>
                                     <h3> {`${historyItem.role}, ${historyItem.organisation}`} </h3>
                                     <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                                     <ul> {historyItem.experiences.map((experience, id) => {
