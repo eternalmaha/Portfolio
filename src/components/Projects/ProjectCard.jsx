bb3e4fed0d519ea044f6ecd4c8465cab3c19be6f
Maha <maha.ahmed4peace@gmail.com>
2024-05-20 17:28:22 -0400

    added class names to projectcard and project components

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import { getImageUrl } from '../../utils';
+import styles from "./ProjectCard.module.css"
 
-export const ProjectCard = ({
-    project: {title, imageSrc, description, skills, demo, source}
-}) => {
+export const ProjectCard = ({ project: {title, imageSrc, description, skills, demo, source}}) => {
     return (
-        <div> 
-            <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} />
-            <h3> {title}</h3>
-            <p>{description}</p>
-            <ul>
+        <div className={styles.container}> 
+            <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image}/>
+            <h3 className={styles.title}> {title}</h3>
+            <p className={styles.description}>{description}</p>
+            <ul className={styles.skills}>
                 {skills.map((skill, id) => {
-                    <li key={id}> {skill} </li>
+                    <li className={styles.skill}  key={id}> {skill} </li> ;
                 })}
             </ul>
-            <div> 
-            <a href={demo}> Demo </a>
-            <a href={source}> Source </a>
+            <div className={styles.links}> 
+                <a className={styles.link} href={demo}> Demo </a>
+                <a className={styles.link} href={source}> Source </a>
             </div>
                         
         </div>
-    )
-}
+    );
+};
