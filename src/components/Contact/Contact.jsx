0340d25109d001471622200f1e31eef0a6480a3a
Maha <maha.ahmed4peace@gmail.com>
2024-05-21 19:20:07 -0400

    contact section styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import "./Contact.module.css"
+import styles from "./Contact.module.css"
 import { getImageUrl } from "../../utils";
 
 export const Contact = () => {
     return (
         <>
-            <footer>
-                <div>
+            <footer id="contact" className={styles.container}>
+                <div className={styles.text}>
                     <h2>Contact</h2>
                     <p>Feel free to reach out!</p>
                 </div>
-                <ul>
-                    <li>
+                <ul className={styles.links}>
+                    <li className={styles.link}>
                         <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" /> 
                         <a href="malito:maha.ahmed4peace@gmail.com">maha.ahmed4peace@gmail.com </a>
                     </li>
-                    <li>
+                    <li className={styles.link}>
                         <img src={getImageUrl("contact/linkedinIcon.png")} alt="linkedin icon" /> 
                         <a href="https://www.linkedin.com/maha-ahmed3">linkedin.com/maha-ahmed3</a>
                     </li>
-                    <li>
+                    <li className={styles.link}>
                         <img src={getImageUrl("contact/githubIcon.png")} alt="github icon" /> 
                         <a href="https://www.github.com/maha4peace"> github.com/maha4peace </a>
                     </li>
