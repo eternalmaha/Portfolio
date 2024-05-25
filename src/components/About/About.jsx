376696b4c40e37dbae21f2fb6d40627ae37aeb6b
Maha <maha.ahmed4peace@gmail.com>
2024-05-24 23:34:12 -0400

    updated about text details

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,16 +14,15 @@ export const About = () => {
                                 <img className={styles.aboutImg} src = {getImageUrl("about/cursorIcon.png")} alt = "front-end dev icon"/> 
                                 <div className={styles.aboutItemText}>
                                     <p>
-                                        The unique combination of creativity, logic, technology and never running out of new things to discover, drives my excitement and passion for web development. I'm not just a web developer — I'm a digital storyteller, crafting websites that blend creativity with functionality. With a passion for design and a knack for coding, I create vibrant, engaging web experiences that bring your brand to life. 
+                                    I had a childhood fascination for how things work, turning a pyschology passion into a cherised profession in adult psycho - social rehabilitation education. A continued fondness for learning and teaching propelled me into the world of STEM secondary education during a historical era.
                                     </p> 
                                 </div>
                             </li>
                             <li className={styles.aboutItem}>
                                 <img className={styles.aboutImg} src = {getImageUrl("about/serverIcon.png")} alt = "back-end dev icon"/>   
                                 <div className={styles.aboutItemText}>
-                
                                     <p>
-                                    My journey into web development started with a childhood fascination for how things work, and it blossomed into a career where I get to turn ideas into interactive, beautiful realities. Whether it’s a quirky portfolio site, a dynamic e-commerce platform, or a sleek business page, I believe in infusing each project with a bit of whimsy and a lot of heart.
+                                    The emerging impact of web technologies on the learning experiences of my students sparked a personal curiosity into our intimate relationship with digital educational technology. 
                                     </p>
                                 </div>
                             </li>
@@ -32,7 +31,7 @@ export const About = () => {
                                 <div className={styles.aboutItemText} >
                     
                                     <p>
-                                    When I’m not behind the screen, you’ll find me exploring local coffee shops, nature, sketching out new ideas, or playing with my adorable (and occasionally mischievous) cat, Malik. I thrive on collaboration and love working with local small businesses to help them shine online.
+                                    I'm not just a web developer though — I'm a digital storyteller who creates vibrant, engaging web experiences. I thrive on collaboration and love working with local educational and civic technolgies to reimagine learning and community. Let's learn to learn together!
                                     </p>
                                 </div>
                             </li>
